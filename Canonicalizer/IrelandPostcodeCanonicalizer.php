cceb8f94383e589ba4ddc2752c9894a73bcfeca1
Alex Latham <alex@usemarkup.com>
2020-08-05 14:35:03 +0100

    feat: adding Ireland to postcode validation

---
+++ Canonicalizer/IrelandPostcodeCanonicalizer.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace Markup\Addressing\Canonicalizer;
+
+
+class IrelandPostcodeCanonicalizer implements CanonicalizerInterface
+{
+    /**
+     * {@inheritdoc}
+     **/
+    public function canonicalize($input)
+    {
+        $acceptableRegex = '/^([AC-FHKNPRTV-Y]\d{2}|D6W)[0-9AC-FHKNPRTV-Y]{4}/';
+
+        //first, return unmodified any input less than 5 characters long, or anything not a string
+        if (!is_string($input) or strlen($input) < 5) {
+            return $input;
+        }
+
+        //second, remove any spaces, uppercase
+        $unspaced = mb_strtoupper(preg_replace('/\s/', '', $input) ?? '', 'UTF-8');
+
+
+        //now check against the regex, and return unmodified if it doesn't pass
+        if (!preg_match($acceptableRegex, $unspaced)) {
+            return $input;
+        }
+
+        //return the modified postcode
+        return $unspaced;
+    }
+}
