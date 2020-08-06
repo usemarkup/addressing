4acb099fcc174a821ac2214bf2dae46955bf49bf
Alex Latham <alex@usemarkup.com>
2020-08-06 11:22:24 +0100

    add validation

--- Canonicalizer/IrelandPostcodeCanonicalizer.php
+++ Canonicalizer/IrelandPostcodeCanonicalizer.php
@@ -11,7 +11,7 @@ class IrelandPostcodeCanonicalizer implements CanonicalizerInterface
      **/
     public function canonicalize($input)
     {
-        $acceptableRegex = '/^([AC-FHKNPRTV-Y]\d{2}|D6W)[0-9AC-FHKNPRTV-Y]{4}/';
+        $acceptableRegex = '/^([AC-FHKNPRTV-Y]\d{2}|D6W) [0-9AC-FHKNPRTV-Y]{4}/';
 
         //first, return unmodified any input less than 5 characters long, or anything not a string
         if (!is_string($input) or strlen($input) < 5) {
@@ -20,14 +20,15 @@ class IrelandPostcodeCanonicalizer implements CanonicalizerInterface
 
         //second, remove any spaces, uppercase
         $unspaced = mb_strtoupper(preg_replace('/\s/', '', $input) ?? '', 'UTF-8');
+        $spaced = sprintf('%s %s', substr($unspaced, 0, -4), substr($unspaced, -4));
 
 
         //now check against the regex, and return unmodified if it doesn't pass
-        if (!preg_match($acceptableRegex, $unspaced)) {
+        if (!preg_match($acceptableRegex, $spaced)) {
             return $input;
         }
 
         //return the modified postcode
-        return $unspaced;
+        return $spaced;
     }
 }
